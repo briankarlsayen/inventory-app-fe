405d7f4fd411519344cbd9cd315b4c5108d45515
blu3fire89 <blu3fire89@gmail.com>
2025-07-23 17:15:04 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>I am the map</h1>
+    <h2>I am the map</h2>
+    <h3>I am the map</h3>
+    <h4>I am the map</h4>
+    <p>I am the map</p>
+    <button class="btn btn-danger">start</button>
+  </div>
+</template>
